5e1d7ff17ad3b7fe2e90fdcc2eb3eb1564a956ee
apavazza <115418293+apavazza@users.noreply.github.com>
2025-02-02 23:42:39 +0100

    Fixed profile page not rendering correctly

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import type { Metadata } from "next";
 import LineChart from "./_components/lineChart";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -13,24 +14,29 @@ export default async function ProfilePage() {
 
   if (!session) {
     return (
-      <main className="flex min-h-screen flex-col items-center py-14 space-y-14 bg-brand-black">
+      <main className="flex min-h-screen flex-col items-center py-14 space-y-14">
         <h1 className="text-6xl font-extrabold tracking-tight">Dashboard</h1>
         <p className="text-xl text-center text-brand-text-weak">
           You need to be logged in to view this page. Please sign in to
           continue.
         </p>
+        <Link 
+          href={'/signin'} 
+          className="border border-brand-primary text-brand-primary px-4 py-2 hover:bg-brand-primary hover:text-white">
+            Sign In</Link>
+      </main>
+    );
+  } else{
+    return (
+      <main className="flex min-h-screen flex-col items-center p-14">
+        <h1 className="text-3xl font-extrabold tracking-tight">
+          Welcome{" "}
+          <span className="text-brand-primary">{session.user.name}</span>
+          <div className="w-full">
+            <LineChart></LineChart>
+          </div>
+        </h1>
       </main>
     );
   }
-  return (
-    <main className="flex min-h-screen flex-col items-center p-14">
-      <h1 className="text-3xl font-extrabold tracking-tight">
-        Welcome{" "}
-        <span className="text-brand-primary">{session.user.name}</span>
-        <div className="w-full">
-          <LineChart></LineChart>
-        </div>
-      </h1>
-    </main>
-  );
 }
